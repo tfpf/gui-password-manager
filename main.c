f23801ef663ac05f96f7075cd1283eed0fa70edb
34point5 <vpaijc@gmail.com>
2020-03-24 11:05:06 +0530

    separating into functions

--- main.c
+++ main.c
@@ -1,14 +1,35 @@
 #include <gtk/gtk.h>
+#include <stdlib.h>
+
+///////////////////////////////////////////////////////////////////////////////
+
+int request_passphrase(void)
+{
+	// create window
+	GtkWidget *window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
+	gtk_window_set_title(GTK_WINDOW(window), "Enter Passphrase");
+
+	// create label
+	GtkWidget *label = gtk_label_new("Water is wet!");
+	gtk_container_add(GTK_CONTAINER(window), label);
+	gtk_widget_show_all(window);
+	g_signal_connect(window, "destroy", G_CALLBACK(gtk_main_quit), NULL);
+	gtk_main();
+}
+
+///////////////////////////////////////////////////////////////////////////////
 
 int main(int const argc, char const *argv[])
 {
-	GtkWidget *window;
-	// gtk_init(&argc, &argv);
+	// must be called before any other GTK functions
+	// normally, arguments are pointers to `argc' and `argv'
 	gtk_init(0, NULL);
-	window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
-	gtk_widget_show(window);
-	g_signal_connect(window, "destroy", G_CALLBACK(gtk_main_quit), NULL);  
-	gtk_main();
+
+	// if user fails to log in, quit
+	if(request_passphrase() == EXIT_FAILURE)
+	{
+		return EXIT_FAILURE;
+	}
 
 	return 0;
 }
