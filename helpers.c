5b34743d74f5a58a117d415ff7b5dbf9aaf9b8fe
34point5 <vpaijc@gmail.com>
2020-03-30 01:36:13 +0530

    bettwe explanation of digest_to_hexdigest

--- helpers.c
+++ helpers.c
@@ -1,8 +1,14 @@
-#include <openssl/sha.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <time.h>
+
+///////////////////////////////////////////////////////////////////////////////
+
+// prototypes
+void wait(int unsigned delay);
+gboolean hide_tooltip(gpointer data);
+void digest_to_hexdigest(char unsigned **p, size_t size);
 
 ///////////////////////////////////////////////////////////////////////////////
 
@@ -15,27 +21,23 @@ void wait(int unsigned delay)
 
 ///////////////////////////////////////////////////////////////////////////////
 
-// read an array of 128 bytes (the array is the SHA512 digest of something)
-// convert this array into an array of 257 bytes
-// the converted array is the SHA512 hexdigest of that same something
-void digest_to_hexdigest(char unsigned **p)
+// input array is a digest (possibly a hash value) of `size' bytes
+// each byte in it can be represented using two hexadecimal characters
+// hence, the corresponding hexdigest will be `2 * size + 1' characters long
+// the extra character is the null character at the end
+void digest_to_hexdigest(char unsigned **p, size_t size)
 {
-	// `digest' is a 128-byte array
-	// `hexdigest' is hexadecimal form of the numerical value of `digest'
-	// `hexdigest' it will be 2 * 128 + 1 = 257 bytes long
-	// each byte in `digest' will be displayed as two hex characters
-	// plus one extra byte at the end, for the null character
 	char unsigned *digest = *p;
-	char unsigned *hexdigest = malloc((2 * SHA512_DIGEST_LENGTH + 1) * sizeof *hexdigest);
+	char unsigned *hexdigest = malloc((2 * size + 1) * sizeof *hexdigest);
 
 	// for each byte in `digest', write two bytes in `hexdigest'
-	for(int i = 0; i < SHA512_DIGEST_LENGTH; ++i)
+	for(size_t i = 0; i < size; ++i)
 	{
 		sprintf((char *)hexdigest + 2 * i, "%02x", digest[i]);
 	}
 
 	// make the argument point to `hexdigest'
-	memset(digest, 0, SHA512_DIGEST_LENGTH);
+	memset(digest, 0, size);
 	*p = hexdigest;
 }
 
